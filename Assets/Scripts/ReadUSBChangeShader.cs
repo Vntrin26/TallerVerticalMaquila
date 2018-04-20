e792e9c1f61b27096c1257585da9af88e498e615
Unknown <tartarknight@gmail.com>
2018-04-20 16:21:55 -0600

    hahaha

---
+++ Assets/Scripts/ReadUSBChangeShader.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ReadUSBChangeShader : MonoBehaviour {
+
+    private Renderer holoRenderer;
+
+    SerialPort sp; //whatever COM arduino uses
+
+    //[SerializeField]
+    //private Shader shaders;
+
+    Shader red;
+    Shader yellow;
+
+    private void Awake()
+    {
+        try
+        {
+            sp = new SerialPort("COM3", 9600);
+        }
+        catch (System.Exception)
+        {
+
+        }
+    }
+
+    void Start()
+    {
+        holoRenderer = GetComponent<Renderer>();
+        red = Shader.Find("Custom/red");
+        yellow = Shader.Find("Custom/yellow");
+
+        try
+        {
+            sp.Open();
+            sp.ReadTimeout = 1;
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("No se inicio la conexión: "+e.ToString());
+
+        }
+
+    }
+
+    int line;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (sp.IsOpen)
+        {
+            try
+            {
+                // DO WHATEVER THE *YOU WANT
+                line = sp.ReadByte();//sp.ReadChar();
+                if (((int)'1').Equals(line))
+                {
+                    //rojo
+                    holoRenderer.material.shader = red;
+                    holoRenderer.material.SetColor("_Color", Color.red);
+
+                }
+                if (((int)'0').Equals(line))
+                {
+                    //amarillo
+                    holoRenderer.material.shader = yellow;
+                    holoRenderer.material.SetColor("_Color", Color.yellow);
+
+                }
+                //Shader.
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                //Do something
+                Debug.Log("lee alterno");
+            }
+        }
+    }
+
+
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-TallerVerticalMaquila
+# MaquilaTallerVertical
+
+
+"Trabajo realizado en el Taller Vertical ITIC/ITC en el 
+ITESM Campus Chihuahua el cual hecho o compuesto de los siguientes componentes:"
+* Arduino UNO
+* Blender
+* Unity
+* Visual Studio 2017
+
+
+## Equipo:
+* Daniel Ventura - A01187731
+* Carlos Enrique Prieto - A01566177
+* Oscar Francisco López Escobar - A01561676
+* Josué Morales - A01187099
+* Jesus Enrique Ruiz Aguilar - A00758756
+
+![Fotos](https://imgur.com/a/gPYpxbM "Fotos")
